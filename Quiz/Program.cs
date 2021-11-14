dd6806eb70b66f29ba2d246900d96d01065b7439
marci <marci@LAPTOP-TM8RLVVF>
2021-11-14 05:59:47 +0100

    Dodaj pliki projektów.

---
+++ Quiz/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Quiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pytanie pytanie1 = new Pytanie();
+           Console.WriteLine("Witaj w Quizie")
+           Console.WriteLine("Pytanie za 500")
+           Console.WriteLine("Jak miał na imię Einstain")
+           Console.WriteLine("Pytanie za 500")
+           Console.WriteLine("Pytanie za 500")
+           //pytanie1.Tresc = "Jak miał na imię Einstain";
+           Console.ReadLine();
+        }
+    }
+}
---
+++ Quiz/Pytanie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz
+{
+   public class Pytanie
+    {
+      public int Id { get; set; }
+      public int Kategoria { get; set; }
+      public string Tresc { get; set; }
+      public string Odpowiedz1 { get; set; }
+      public string Odpowiedz2 { get; set; }
+      public string Odpowiedz3 { get; set; }
+      public string Odpowiedz4 { get; set; }
+
+    }
+}
